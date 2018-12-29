01d19aa73e642ac78dab12c98634591b6921ac41
aquariuz <gpetiaa@gmail.com>
2018-12-29 19:33:37 +0200

    registration done

--- src/CrochetLibraryBundle/Controller/DefaultController.php
+++ src/CrochetLibraryBundle/Controller/DefaultController.php
@@ -9,12 +9,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="dashboard")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('default/dashboard.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
---
+++ src/CrochetLibraryBundle/Form/UserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CrochetLibraryBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('username',TextType::class)
+            ->add('password', TextType::class)
+            ->add('name', TextType::class)
+            ->add('portfolio', TextType::class)
+            ->add('picture', FileType::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CrochetLibraryBundle\Entity\User'
+        ));
+    }
+}
